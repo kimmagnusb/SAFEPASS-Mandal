027f829ee69b6fb65071cc7fa5137cc5b7b87352
Kim Magnus Bærum <kim.barum@nina.no>
2018-02-07 11:29:21 +0100

    Some comments and suggestions

--- Script/2018. 05. Analyse ecohydraulic relationships - angular difference.r
+++ Script/2018. 05. Analyse ecohydraulic relationships - angular difference.r
@@ -1,4 +1,3 @@
-
 ############
 # CREATED: 2018-02-04
 ############
@@ -9,8 +8,11 @@ library(geoR)
 library(nlme)
 library(lattice)
 library(colorRamps)
+library(RCurl)
+# reading data directly from github
 
-DIR<-"M:\\My Documents\\NINA projects\\Safepass\\Data2018\\Processed\\"
+x <- getURL("https://raw.githubusercontent.com/kimmagnusb/SAFEPASS-Mandal/master/Data/NewSmoltData%20for%20LME.2018-02-06.csv")
+D <- read.csv(text=x)
 
 # CORVIF FUNCTION IS FROM ZUUR ET AL.
 #Library files for courses provided by: Highland Statistics Ltd.
@@ -76,15 +78,15 @@ myvif <- function(mod) {
 ############
 
 Transform2Normal.2p<-function(Ori){
-T.box = boxcoxfit(Ori, lambda2=TRUE)
-lambda = T.box$lambda[1]
-lambda2 = T.box$lambda[2]
-if(lambda==0){T.norm = log(Ori + lambda2)}
-if(lambda!=0){T.norm = ((Ori + lambda2) ^ lambda - 1) / lambda}
-return(T.norm)
+  T.box = boxcoxfit(Ori, lambda2=TRUE)
+  lambda = T.box$lambda[1]
+  lambda2 = T.box$lambda[2]
+  if(lambda==0){T.norm = log(Ori + lambda2)}
+  if(lambda!=0){T.norm = ((Ori + lambda2) ^ lambda - 1) / lambda}
+  return(T.norm)
 }
 
-D <- read.csv(paste(DIR,"NewSmoltData for LME.2018-02-04.csv",sep=""))
+#D <- read.csv("smolt_data.csv",header=T,sep="")
 
 
 D$PosErr<-apply(cbind(D$PosErrX,D$PosErrY),1,mean,na.rm=TRUE)
@@ -114,33 +116,47 @@ boxplot(x,ylab="Angular difference",xlab="Flow speed",main="3D grid")
 
 ### LME
 
-tf<-!is.na(D$AngDif); D<-D[tf,]
+tf<-!is.na(D$AngDif.A); D<-D[tf,]
 
-D$AngDif.norm<-Transform2Normal.2p(D$AngDif)
+D$AngDif.norm.A<-Transform2Normal.2p(D$AngDif.A)
+
+tf2<-!is.na(D$AngDif.B); D<-D[tf2,]
+
+D$AngDif.norm.B<-Transform2Normal.2p(D$AngDif.B)
+
+tf3<-!is.na(D$AngDif.C); D<-D[tf3,]
+
+D$AngDif.norm.C<-Transform2Normal.2p(D$AngDif.C)
+
+hist(D$AngDif.norm.A)
+hist(D$AngDif.norm.B)
+hist(D$AngDif.norm.C)
+# Doesnt really look normal..
+
 
 tf<-colnames(D)%in%c("FlowSpeed","ke","Len","FF")
 Pred<-D[,tf]
 corvif(Pred)
 
 # FIT ALL PREDICTORS
-g1<-lme(AngDif.norm~FlowSpeed+ke+Len+FF, weights=~Wt, random=~1 | Tag, method="ML", data=D)
+g1<-lme(AngDif.norm.A~FlowSpeed+ke+Len+FF, weights=~Wt, random=~1 | Tag, method="ML", data=D)
 summary(g1)$tTable
 
 # REMOVE FF BECAUSE IT IS NOT STRONGLY SIGNIFICANT
-g2<-lme(AngDif.norm~FlowSpeed+ke+Len, weights=~Wt, random=~1 | Tag, method="ML", data=D)
+g2<-lme(AngDif.norm.A~FlowSpeed+ke+Len, weights=~Wt, random=~1 | Tag, method="ML", data=D)
 summary(g2)$tTable
 
 AIC(g1,g2)
 
 # EXAMINE INTERACTION BETWEEN KE AND LEN
-g3<-lme(AngDif.norm~FlowSpeed+ke*Len, weights=~Wt, random=~1 | Tag, method="ML", data=D)
+g3<-lme(AngDif.norm.A~FlowSpeed+ke*Len, weights=~Wt, random=~1 | Tag, method="ML", data=D)
 summary(g3)$tTable
 
-AIC(g1,g2,g3)
+AIC(g1,g2,g3,g)
 
 
 # PREDICT FROM MODEL
-g<-lme(AngDif.norm~FlowSpeed+FlowSpeed:ke, weights=~Wt, random=~1 | Tag, method="ML", data=D)
+g<-lme(AngDif.norm.A~FlowSpeed+FlowSpeed:ke, weights=~Wt, random=~1 | Tag, method="ML", data=D) # why not g3, seems better? Also, at least in my head, difficult to interpret without the main effect of ke.
 summary(g)
 
 ke<-seq(0,0.02,0.0001)
@@ -170,4 +186,55 @@ gg2<-lme(AngDif.norm~FlowSpeed+ke+Len, weights=~Wt, random=~1 | Tag, correlation
 gg3<-lme(AngDif.norm~FlowSpeed+ke*Len, weights=~Wt, random=~1 | Tag, correlation = corAR1(), method="ML", data=D)
 
 AIC(gg1,gg2,gg3)
+
+
+##########################
+# Messing around, starring Kim Magnus
+##Using lme4, just because I'm more used to the structure in the package (nlme is much easier to use when it comes to incorporate autocorr structure) 
+##########################
+library(lme4)
+library(MuMIn)
+# Checking dist
+hist(D$AngDif.A)
+hist(D$AngDif.B)
+hist(D$AngDif.C)
+## All look rather similar, stomack feeling says non-linear response. Note to self, check model assumptions 
+
+#check assumptions of best lme mod
+plot(g3) # well...think we need to consider what data goes in to the model
+
+qqnorm(resid(g3)) # not to bad from the quick look, but again with the data used
+qqnorm(g3, ~ranef(., level=1))
+
+
+#quick and dirty...no weights for now, think a better method might be to select out this apriori?
+mod.lmer<-lmer(AngDif.norm.A~FlowSpeed*ke*Len +(1|Tag), data=D)
+options(na.action = "na.fail")
+dmod<-dredge(mod.lmer)
+dmod #fuck, complicated model with all the weights
+
+#reduce complexity in global model
+mod.lmer2<-lmer(AngDif.norm.A~FlowSpeed+ke+Len+FlowSpeed:ke+FlowSpeed:Len+ke:Len +(1|Tag), data=D)
+dmod2<-dredge(mod.lmer2,evaluate=T) # Should do something about the very different scales
+dmod2 # Aaah, better
+
+top.models <- get.models(dmod2, cumsum(weight) <= .95)
+mod.lmer.avg<-model.avg(top.models,fit = TRUE)
+
+# PREDICT FROM MODEL
+
+ke<-seq(0,0.02,0.0001)
+FlowSpeed<-seq(0,0.72,0.001)
+Len<-seq(12,18,1)
+new.dat <- expand.grid(FlowSpeed=FlowSpeed,ke=ke, Len=Len, Tag=0)
+gg<-predict(mod.lmer.avg,newdata=new.dat) #hmm, doent work needs to check this after lunch
+new.dat<-data.frame(new.dat)
+new.dat$gg<-gg
+
+par(mfrow=c(1,1))
+plot(new.dat$FlowSpeed,new.dat$ke,pch=15,cex=2,col=new.dat$colvec)
+points(D$FlowSpeed,D$ke,pch=1,cex=0.5)
+
+colvec<-blue2green2red(100); colvec=colvec[1:99]
+levelplot(gg ~ FlowSpeed * ke, data = new.dat,cuts = 98, region = TRUE,col.regions=colvec)
 
